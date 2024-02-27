22a9412f6037b4349411e33fccd72c9018a7be4b
Bryce Schultz <bryceschultz26@gmail.com>
2024-02-26 23:04:45 -0500

    adding prettier and husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
         "es2021": true,
         "node": true
     },
-    "extends": "standard-with-typescript",
+    "extends": ["standard-with-typescript", "prettier"],
     "overrides": [
         {
             "env": {
@@ -21,6 +21,7 @@ module.exports = {
         "ecmaVersion": "latest",
         "sourceType": "module"
     },
+    "ignorePatterns": ['.eslintrc.js'],
     "rules": {
     }
 }
